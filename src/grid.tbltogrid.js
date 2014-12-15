ff6138f8d0db5efd55c085a48397a71770fa93c9
flack <flack@contentcontrol-berlin.de>
2014-12-15 22:26:58 +0100

    Change formatting & fix some jshint warnings

--- src/grid.tbltogrid.js
+++ src/grid.tbltogrid.js
@@ -1,12 +1,12 @@
 /*
- Transform a table to a jqGrid.
- Peter Romianowski <peter.romianowski@optivo.de> 
- If the first column of the table contains checkboxes or
- radiobuttons then the jqGrid is made selectable.
+  Transform a table to a jqGrid.
+  Peter Romianowski <peter.romianowski@optivo.de>
+  If the first column of the table contains checkboxes or
+  radiobuttons then the jqGrid is made selectable.
 */
 // Addition - selector can be a class or id
 function tableToGrid(selector, options) {
-jQuery(selector).each(function() {
+    jQuery(selector).each(function() {
 	if(this.grid) {return;} //Adedd from Tony Tomov
 	// This is a small "hack" to make the width of the jqGrid 100%
 	jQuery(this).width("99%");
@@ -24,44 +24,44 @@ jQuery(selector).each(function() {
 	var colModel = [];
 	var colNames = [];
 	jQuery('th', jQuery(this)).each(function() {
-		if (colModel.length === 0 && selectable) {
-			colModel.push({
-				name: '__selection__',
-				index: '__selection__',
-				width: 0,
-				hidden: true
-			});
-			colNames.push('__selection__');
-		} else {
-			colModel.push({
-				name: jQuery(this).attr("id") || jQuery.trim(jQuery.jgrid.stripHtml(jQuery(this).html())).split(' ').join('_'),
-				index: jQuery(this).attr("id") || jQuery.trim(jQuery.jgrid.stripHtml(jQuery(this).html())).split(' ').join('_'),
-				width: jQuery(this).width() || 150
-			});
-			colNames.push(jQuery(this).html());
-		}
+	    if (colModel.length === 0 && selectable) {
+		colModel.push({
+		    name: '__selection__',
+		    index: '__selection__',
+		    width: 0,
+		    hidden: true
+		});
+		colNames.push('__selection__');
+	    } else {
+		colModel.push({
+		    name: jQuery(this).attr("id") || jQuery.trim(jQuery.jgrid.stripHtml(jQuery(this).html())).split(' ').join('_'),
+		    index: jQuery(this).attr("id") || jQuery.trim(jQuery.jgrid.stripHtml(jQuery(this).html())).split(' ').join('_'),
+		    width: jQuery(this).width() || 150
+		});
+		colNames.push(jQuery(this).html());
+	    }
 	});
 	var data = [];
 	var rowIds = [];
 	var rowChecked = [];
 	jQuery('tbody > tr', jQuery(this)).each(function() {
-		var row = {};
-		var rowPos = 0;
-		jQuery('td', jQuery(this)).each(function() {
-			if (rowPos === 0 && selectable) {
-				var input = jQuery('input', jQuery(this));
-				var rowId = input.attr("value");
-				rowIds.push(rowId || data.length);
-				if (input.is(":checked")) {
-					rowChecked.push(rowId);
-				}
-				row[colModel[rowPos].name] = input.attr("value");
-			} else {
-				row[colModel[rowPos].name] = jQuery(this).html();
-			}
-			rowPos++;
-		});
-		if(rowPos >0) { data.push(row); }
+	    var row = {};
+	    var rowPos = 0;
+	    jQuery('td', jQuery(this)).each(function() {
+		if (rowPos === 0 && selectable) {
+		    var input = jQuery('input', jQuery(this));
+		    var rowId = input.attr("value");
+		    rowIds.push(rowId || data.length);
+		    if (input.is(":checked")) {
+			rowChecked.push(rowId);
+		    }
+		    row[colModel[rowPos].name] = input.attr("value");
+		} else {
+		    row[colModel[rowPos].name] = jQuery(this).html();
+		}
+		rowPos++;
+	    });
+	    if(rowPos >0) { data.push(row); }
 	});
 
 	// Clear the original HTML table
@@ -71,36 +71,36 @@ jQuery(selector).each(function() {
 	jQuery(this).addClass("scroll");
 
 	jQuery(this).jqGrid(jQuery.extend({
-		datatype: "local",
-		width: w,
-		colNames: colNames,
-		colModel: colModel,
-		multiselect: selectMultiple
-		//inputName: inputName,
-		//inputValueCol: imputName != null ? "__selection__" : null
+	    datatype: "local",
+	    width: w,
+	    colNames: colNames,
+	    colModel: colModel,
+	    multiselect: selectMultiple
+	    //inputName: inputName,
+	    //inputValueCol: imputName != null ? "__selection__" : null
 	}, options || {}));
 
 	// Add data
 	var a;
 	for (a = 0; a < data.length; a++) {
-		var id = null;
-		if (rowIds.length > 0) {
-			id = rowIds[a];
-			if (id && id.replace) {
-				// We have to do this since the value of a checkbox
-				// or radio button can be anything 
-				id = encodeURIComponent(id).replace(/[.\-%]/g, "_");
-			}
+	    var id = null;
+	    if (rowIds.length > 0) {
+		id = rowIds[a];
+		if (id && id.replace) {
+		    // We have to do this since the value of a checkbox
+		    // or radio button can be anything
+		    id = encodeURIComponent(id).replace(/[.\-%]/g, "_");
 		}
-		if (id === null) {
-			id = a + 1;
-		}
-		jQuery(this).jqGrid("addRowData",id, data[a]);
+	    }
+	    if (id === null) {
+		id = a + 1;
+	    }
+	    jQuery(this).jqGrid("addRowData",id, data[a]);
 	}
 
 	// Set the selection
 	for (a = 0; a < rowChecked.length; a++) {
-		jQuery(this).jqGrid("setSelection",rowChecked[a]);
+	    jQuery(this).jqGrid("setSelection",rowChecked[a]);
 	}
-});
-};
+    });
+}
