6b4684dc6d2cde401760d849f9f2b87acac11791
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2015-01-15 16:24:46 +0100

    Oleg change - small bug fixes in getGridRowById and setSelection

--- js/grid.base.js
+++ js/grid.base.js
@@ -3427,6 +3427,9 @@ $.jgrid.extend({
 		});
 	},
 	getGridRowById: function ( rowid ) {
+		if (rowid == null) {
+			return null;
+		}
 		var row, rowId = rowid.toString();
 		this.each( function(){
 			var i, rows = this.rows, tr;
@@ -3442,6 +3445,7 @@ $.jgrid.extend({
 				}
 			} catch ( e ) {
 				row = $(this.grid.bDiv).find( "#" + $.jgrid.jqID( rowid ));
+				row = row.length > 0 ? row[0] : null;
 			}
 		});
 		return row;
@@ -3492,9 +3496,11 @@ $.jgrid.extend({
 			if(!p.multiselect) {	
 				if(pt.className !== "ui-subgrid") {
 					if( p.selrow !== pt.id ) {
-						csr = $($t).jqGrid('getGridRowById', p.selrow);
-						if( csr ) {
-							$(  csr ).removeClass("ui-state-highlight").attr({"aria-selected":"false", "tabindex" : "-1"});
+						if (p.selrow !== null) {
+							csr = $($t).jqGrid('getGridRowById', p.selrow);
+							if( csr ) {
+								$(  csr ).removeClass("ui-state-highlight").attr({"aria-selected":"false", "tabindex" : "-1"});
+							}
 						}
 						$(pt).addClass("ui-state-highlight").attr({"aria-selected":"true", "tabindex" : "0"});//.focus();
 						if(fid) {
