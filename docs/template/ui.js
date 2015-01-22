1bb07aa0bd6f40f4e07dfd798dcfefa9a71b2662
flack <flack@contentcontrol-berlin.de>
2015-01-22 21:33:27 +0100

    Improve layout of demo toolbar slightly & make it accessible to the code snippet being executed

--- docs/template/ui.js
+++ docs/template/ui.js
@@ -10,27 +10,30 @@ $(document).ready(function()
             .hide();
         $('<table id="grid"></table><div id="pager"></div>')
             .insertBefore(container);
-        eval($('pre code.language-javascript').text());
-        $('<label class="btn btn-primary" id="show-code">Show Code</label>')
+        $('<div class="btn-group"></div>')
             .insertBefore(container)
-            .on('click', function()
-            {
-                var button = $(this);
-                if (container.is(':visible'))
-                {
-                    container.slideUp(300, function()
+            .append(
+                $('<label class="btn btn-primary" id="show-code">Show Code</label>')
+                    .on('click', function()
                     {
-                        button.removeClass('active');
-                    });
-                }
-                else
-                {
-                    container.slideDown(300, function()
-                    {
-                        button.addClass('active');
-                    });
-                }
-            });
+                        var button = $(this);
+                        if (container.is(':visible'))
+                        {
+                            container.slideUp(300, function()
+                            {
+                                button.removeClass('active');
+                            });
+                        }
+                        else
+                        {
+                            container.slideDown(300, function()
+                            {
+                                button.addClass('active');
+                            });
+                        }
+                    })
+            );
+        eval($('pre code.language-javascript').text());
     }
 
     if ($('h2, h3, h4').length > 5)
