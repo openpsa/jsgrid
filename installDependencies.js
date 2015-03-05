a5c9b2871c604467118056ec6e97de3d1b8cdd09
Matthew Hutton <meh-uk@users.noreply.github.com>
2015-03-05 09:14:21 +0000

    Updating dependency installation to use package-script to handle the global stuff itself.

---
+++ installDependencies.js
@@ -0,0 +1,11 @@
+require('package-script').spawn([
+      {
+          command: "npm",
+          args: ["install", "-g", "grunt-cli"]
+      },
+      {
+          command: "npm",
+          args: ["install", "-g", "bower"]
+      }
+
+  ]);
